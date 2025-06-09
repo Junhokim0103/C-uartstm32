20906c41885d369aafa944b9a91a9b0e69620f87
Junhokim0103 <93583003+Junhokim0103@users.noreply.github.com>
2025-06-09 12:39:02 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace stm32_serial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 83);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "LED1 Toggle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(334, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 84);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "LED2 Toggle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(214, 199);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(346, 43);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(214, 368);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "P_bar 0%";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(358, 368);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "P_bar 50%";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(485, 368);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(98, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "P_bar 100%";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(36, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(497, 109);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(156, 84);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "LED3 Toggle";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(237, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(237, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(237, 83);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 10;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(214, 249);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(346, 23);
+            this.progressBar2.TabIndex = 11;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(214, 279);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(346, 23);
+            this.progressBar3.TabIndex = 12;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(36, 110);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(105, 23);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Open";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(36, 163);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(105, 23);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "Close";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace stm32_serial
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serial;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.DataSource = SerialPort.GetPortNames();
+
+        }
+        private void serialHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serial.ReadLine();
+            this.Invoke((MethodInvoker)delegate
+            {
+                string[] parts = data.Trim().Split(',');
+                //textBox1.AppendText(data + Environment.NewLine);
+                //textBox1.Text = data;
+                if((parts.Length == 3) && (int.TryParse(parts[0],out int val1))&& (int.TryParse(parts[1], out int val2)) && (int.TryParse(parts[2], out int val3)))
+                //if (int.TryParse(data.Trim(), out int value))
+                {
+                    textBox1.Text = val1.ToString();
+                    textBox2.Text = val2.ToString();
+                    textBox3.Text = val3.ToString();
+                    if (val1 >= progressBar1.Minimum && val1 <= progressBar1.Maximum)
+                    {
+                        progressBar1.Value = val1;
+                    }
+                    if (val2/100 >= progressBar1.Minimum && val2/100 <= progressBar1.Maximum)
+                    {
+                        progressBar2.Value = val2/100;
+                    }
+                    if (val3/100 >= progressBar1.Minimum && val3/100 <= progressBar1.Maximum)
+                    {
+                        progressBar3.Value = val3/100;
+                    }
+                }
+
+            });
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           // Serial_Load();
+            MessageBox.Show("LED1 toggle");
+            //SerialPort serial = new SerialPort(comboBox1.Text,115200);
+            //serial.Open();
+            serial.Write("LED1\n\r");
+            //serial.Close();
+            //SerialPort.o
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("LED2 toggle");
+            //SerialPort serial = new SerialPort(comboBox1.Text, 115200);
+            //serial.Open();
+            serial.Write("LED2\n\r");
+            //serial.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 50;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 100;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("LED3 toggle");
+            //SerialPort serial = new SerialPort(comboBox1.Text, 115200);
+            //serial.Open();
+            serial.Write("LED3\n\r");
+            //serial.Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //open
+            MessageBox.Show("port open");
+       
+            serial = new SerialPort(comboBox1.Text, 115200);
+            serial.DataReceived += new SerialDataReceivedEventHandler(serialHandler);
+            serial.Open();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //close
+            MessageBox.Show("port closed");
+            //SerialPort serial = new SerialPort(comboBox1.Text, 115200);
+            //serial.Open();
+            //serial.Write("LED3\n\r");
+            serial.Close();
+        }
+    }
+}
